2c4772a09b20691c8b7a5c5937944527095f3279
Thomas Lindae <thomas@lindae.de>
2025-09-20 16:27:00 +0200

    set application id (class name in hyprland)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use eframe::egui;
 fn main() -> eframe::Result {
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
+            .with_app_id("chameleos")
             // unfortunately this doesn't work with hyprland :/ (yet?)
             // .with_mouse_passthrough(true)
             // .with_always_on_top()
